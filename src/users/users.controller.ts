0460eee7070e325b4566484ed3f517dbb9d46a32
SamriddhaL <86090897+SamriddhaL@users.noreply.github.com>
2026-02-10 13:40:36 +0545

    add get methods

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -1,4 +1,4 @@
-import { Body, Controller, Post, UsePipes, ValidationPipe } from "@nestjs/common";
+import { Body, Controller, Get, Param, Post, UsePipes, ValidationPipe } from "@nestjs/common";
 import { CreateUserDto } from "./dto/CreateUser.dto";
 import { UsersService } from "./users.service";
 
@@ -9,5 +9,15 @@ export class UsersController{
     @Post()
     createUser(@Body() createUserDto: CreateUserDto){
         return this.userService.createUser(createUserDto)
+    }
+
+    @Get()
+    getUsers(){
+        return this.userService.getUsers()
+    }
+
+    @Get(':id')
+    getUserById(@Param('id') id: string){
+        return this.userService.getUserById(id)
     }
 }
--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -1,6 +1,6 @@
-import { Injectable } from "@nestjs/common";
+import { Injectable, NotFoundException } from "@nestjs/common";
 import { InjectModel } from "@nestjs/mongoose";
-import { Model } from "mongoose";
+import mongoose, { Model } from "mongoose";
 import { Users } from "src/schema/users.schema";
 import { CreateUserDto } from "./dto/CreateUser.dto";
 
@@ -8,8 +8,20 @@ import { CreateUserDto } from "./dto/CreateUser.dto";
 export class UsersService{
     constructor(@InjectModel(Users.name) private UserModel: Model<Users>  ){}
 
-    createUser(createUserDto: CreateUserDto){
-        const newUser = new this.UserModel(createUserDto)
+    async createUser(createUserDto: CreateUserDto){
+        const newUser = await new this.UserModel(createUserDto)
         return newUser.save()
+    }
+
+    async getUsers(){
+        return await this.UserModel.find()
+    }
+
+    async getUserById(id: string){
+        const isValid = mongoose.Types.ObjectId.isValid(id)
+        if (!isValid) throw new NotFoundException()
+        const user = await this.UserModel.findById(id)
+        if (!user) throw new NotFoundException()
+        return user
     }
 }
