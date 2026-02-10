ce88a055635bf3362b0868ef24b37474b0ac9c50
SamriddhaL <86090897+SamriddhaL@users.noreply.github.com>
2026-02-10 14:05:02 +0545

    add update and delete methods

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -1,6 +1,8 @@
-import { Body, Controller, Get, Param, Post, UsePipes, ValidationPipe } from "@nestjs/common";
+import { Body, Controller, Delete, Get, HttpException, Param, Patch, Post, UsePipes, ValidationPipe } from "@nestjs/common";
 import { CreateUserDto } from "./dto/CreateUser.dto";
 import { UsersService } from "./users.service";
+import mongoose from "mongoose";
+import { UpdateUserDto } from "./dto/UpdateUser.dto";
 
 @Controller('users')
 export class UsersController{
@@ -19,5 +21,23 @@ export class UsersController{
     @Get(':id')
     getUserById(@Param('id') id: string){
         return this.userService.getUserById(id)
+    }
+
+    @Patch(':id')
+    async updateUser(@Param('id') id: string, @Body() updateUserDto: UpdateUserDto){
+        const isValid = mongoose.Types.ObjectId.isValid(id);
+        if (!isValid) throw new HttpException('Invalid ID', 400);
+        const updatedUser = await this.userService.updateUser(id, updateUserDto);
+        if (!updatedUser) throw new HttpException('User Not Found', 404);
+        return updatedUser;
+    }
+
+    @Delete(':id')
+    async deleteUser(@Param('id') id: string) {
+        const isValid = mongoose.Types.ObjectId.isValid(id);
+        if (!isValid) throw new HttpException('Invalid ID', 400);
+        const deletedUser = await this.userService.deleteUser(id);
+        if (!deletedUser) throw new HttpException('User Not Found', 404);
+        return;
     }
 }
--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -3,25 +3,34 @@ import { InjectModel } from "@nestjs/mongoose";
 import mongoose, { Model } from "mongoose";
 import { Users } from "src/schema/users.schema";
 import { CreateUserDto } from "./dto/CreateUser.dto";
+import { UpdateUserDto } from "./dto/UpdateUser.dto";
 
 @Injectable()
 export class UsersService{
-    constructor(@InjectModel(Users.name) private UserModel: Model<Users>  ){}
+    constructor(@InjectModel(Users.name) private userModel: Model<Users>  ){}
 
     async createUser(createUserDto: CreateUserDto){
-        const newUser = await new this.UserModel(createUserDto)
+        const newUser = await new this.userModel(createUserDto)
         return newUser.save()
     }
 
     async getUsers(){
-        return await this.UserModel.find()
+        return await this.userModel.find()
     }
 
     async getUserById(id: string){
         const isValid = mongoose.Types.ObjectId.isValid(id)
         if (!isValid) throw new NotFoundException()
-        const user = await this.UserModel.findById(id)
+        const user = await this.userModel.findById(id)
         if (!user) throw new NotFoundException()
         return user
+    }
+
+    updateUser(id: string, updateUserDto: UpdateUserDto) {
+        return this.userModel.findByIdAndUpdate(id, updateUserDto, { new: true });
+    }
+
+    deleteUser(id: string) {
+        return this.userModel.findByIdAndDelete(id);
     }
 }
